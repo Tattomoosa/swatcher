6047f4ac01c24b0d1bab73d1fa33cf2daacbf93d
Matt O'Tousa <matt@mattotousa.com>
2019-05-29 00:33:51 -0700

    button fix

--- swatches.e31bb0bc.js
+++ swatches.e31bb0bc.js
@@ -1267,7 +1267,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33321" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41641" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
